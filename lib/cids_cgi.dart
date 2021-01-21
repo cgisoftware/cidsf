6510801dd5faf426a2ed75c1fe0b06e34d065589
Vinicius Boscardin <boscardinvinicius@gmail.com>
2021-01-21 15:04:20 -0300

    .

--- lib/cids_cgi.dart
+++ lib/cids_cgi.dart
@@ -4,7 +4,7 @@ export 'src/core/domain/device/handler/shared_preferences_handler.dart';
 export 'src/module/settings/domain/usecase/seguranca_usecase.dart';
 export 'src/core/domain/device/handler/biometrics_handler.dart';
 export 'src/module/settings/page/firebase_validation_page.dart';
-export 'src/core/domain/device/handler/firebase_handler.dart';
+export 'src/core/domain/device/handler/cids_handler.dart';
 export 'src/core/domain/device/handler/dialog_handler.dart';
 export 'src/core/domain/device/handler/error_handler.dart';
 export 'src/module/auth/page/biometrics_error_page.dart';
