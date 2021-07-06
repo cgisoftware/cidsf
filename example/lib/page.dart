61da6244e2fa0d5c576c2ff666e516ad172b157b
sergiotessaro <sergiotessaro1233@gmail.com>
2021-07-06 10:23:02 -0300

    fix no loading infinito ao salvar e dar erro na tela de configurações

--- example/lib/page.dart
+++ example/lib/page.dart
@@ -1,22 +1,17 @@
-
-
 import 'package:cids_cgi/cids_cgi.dart';
 import 'package:cids_cgi_example/main.dart';
 import 'package:cids_cgi_example/validaLogin.dart';
 import 'package:flutter/material.dart';
 
 final biometricsPage = BiometricsErrorPage(
-  biometricsHandler: biometricsHandler,
-  context: (context) {
-    biometricsContext = context;
-  }
-);
+    biometricsHandler: biometricsHandler,
+    context: (context) {
+      biometricsContext = context;
+    });
 
-final homePage = MyHome(
-  context: (context) {
-    biometricsContext = context;
-  }
-);
+final homePage = MyHome(context: (context) {
+  biometricsContext = context;
+});
 
 final authPage = AuthPage(
   validaLogin: ValidaLogin().call,
@@ -28,6 +23,7 @@ final authPage = AuthPage(
 );
 
 final settingsPage = SettingsPage(
+  validaLogin: ValidaLogin().call,
   appBarColor: Colors.red,
   appBarTextColor: Colors.white,
 );
