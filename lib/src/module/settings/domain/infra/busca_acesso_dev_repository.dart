d1a5d78d2498f05821b35ed89d2bff3b1d2c02bb
Alexandre Gayer da Silva <alexandregs119@hotmail.com>
2023-08-25 11:43:36 -0300

    Atualização Biometric Handler - removido AppLifecycleState.hidden

--- lib/src/module/settings/domain/infra/busca_acesso_dev_repository.dart
+++ lib/src/module/settings/domain/infra/busca_acesso_dev_repository.dart
@@ -11,23 +11,19 @@ class BuscaAcessoDevRepository {
       final handler = SharedPreferencesHandler();
       final chave = await handler.getChaveDev();
       final usePacificUrl = await handler.usePacificUrl();
-      Response response =
-          await this.dio.get("https://desenvapi.cgisoftware.com.br/acesso-app/chave/$codCliCgi?apiToken=$chave");
+      Response response = await this.dio.get("https://desenvapi.cgisoftware.com.br/acesso-app/chave/$codCliCgi?apiToken=$chave");
 
       final acessoApp = AcessoApp.fromJson(response.data);
 
-      await handler.set("edtServico",
-          usePacificUrl ? acessoApp.pacificUrl! : acessoApp.proxyUrl!);
+      await handler.set("edtServico", usePacificUrl ? acessoApp.pacificUrl! : acessoApp.proxyUrl!);
       await handler.set("numDevices", acessoApp.qtdDevices.toString());
-      await handler.set(
-          "numDevicesVendedor", acessoApp.qtdDevicesPorUsuario.toString());
+      await handler.set("numDevicesVendedor", acessoApp.qtdDevicesPorUsuario.toString());
       await handler.set("usuarioP", acessoApp.usuario.toString());
       await handler.set("senhaP", acessoApp.senha.toString());
       await handler.set("versao_minima", acessoApp.versaoMinima.toString());
       return "";
-    } on DioError catch (e) {
-      return e.response?.data.toString() ??
-          "Não conseguimos verificar as informações no nosso Proxy!";
+    } on DioException catch (e) {
+      return e.response?.data.toString() ?? "Não conseguimos verificar as informações no nosso Proxy!";
     } catch (e) {
       return "Não conseguimos verificar as informações no nosso Proxy!";
     }
